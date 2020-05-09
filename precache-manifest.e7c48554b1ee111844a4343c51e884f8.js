3523194d9494570c9179dbb72cf5bf6d61796821
andersonfrfilho <andersonfrfilho@gmail.com>
2020-05-09 19:42:11 -0300

    Updates

---
+++ precache-manifest.e7c48554b1ee111844a4343c51e884f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fa0dd0423af0ff00d72d458f827dd2",
+    "url": "/oplab/index.html"
+  },
+  {
+    "revision": "5dbc4b053a3b7e526128",
+    "url": "/oplab/static/js/2.834a160e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oplab/static/js/2.834a160e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac2dd0ce0e4d556301f",
+    "url": "/oplab/static/js/main.5af554cf.chunk.js"
+  },
+  {
+    "revision": "245ae6bb37b5f09d2ea3",
+    "url": "/oplab/static/js/runtime-main.57bceb89.js"
+  },
+  {
+    "revision": "e3a7cb838e784763f00840666cbf3602",
+    "url": "/oplab/static/media/logo.e3a7cb83.png"
+  },
+  {
+    "revision": "cf84d23b937d7bcda25ac57efc8ccf2f",
+    "url": "/oplab/static/media/sign-up-background.cf84d23b.png"
+  }
+]);
